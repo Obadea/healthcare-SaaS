71dd0b3d0b4c9bfc30be9e4cbdedf6422222efe2
obadea <obadeagbenga@gmail.com>
2025-10-21 15:34:26 +0100

    fix: sentry bug

--- app/api/sentry-example-api/route.ts
+++ app/api/sentry-example-api/route.ts
@@ -9,6 +9,9 @@ class SentryExampleAPIError extends Error {
 }
 // A faulty API route to test Sentry's error monitoring
 export function GET() {
-  throw new SentryExampleAPIError("This error is raised on the backend called by the example page.");
+  throw new SentryExampleAPIError(
+    "This error is raised on the backend called by the example page."
+  );
+  // eslint-disable-line
   return NextResponse.json({ data: "Testing Sentry Error..." });
 }
